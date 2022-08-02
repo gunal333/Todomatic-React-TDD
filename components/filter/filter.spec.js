c6597ffb0307b4465ff5a661ff91b7b516b0e4a2
gunal333 <p.gunal333@gmail.com>
2022-08-02 17:36:03 +0530

    Tests are added for filter component

---
+++ components/filter/filter.spec.js
@@ -0,0 +1,29 @@
+import { shallow } from "enzyme";
+import React from "react";
+
+describe("Basic rendering of filter option",()=>{
+    it("Should render the filter option",()=>{
+        
+    });
+    it("Should have all option in the filter",()=>{
+
+    });
+    it("Should have completed option in the filter",()=>{
+
+    });
+    it("Should have uncompleted option in the filter",()=>{
+
+    });
+});
+
+describe("Testing filter functionality",()=>{
+    it("Should show only completed todo task when clicked on completed option",()=>{
+
+    });
+    it("Should show only uncompleted todo task when clicked on uncompleted option",()=>{
+        
+    });
+    it("Should show only all todo task when clicked on all option",()=>{
+        
+    });
+});
